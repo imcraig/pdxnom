f6d0f974e18aa5bb29aaab17af2632c9d1cf3a3b
Jonathan "Duke" Leto <jonathan@leto.net>
2011-04-21 00:33:38 -0700

    work in progress to calculate closest marker

--- js/pdxnom.js
+++ js/pdxnom.js
@@ -1,5 +1,9 @@
+// pdxnom UI code
+
 var directionsService = new google.maps.DirectionsService();
 var directionsDisplay = new google.maps.DirectionsRenderer();
+
+// Cart Pods, these should be stored in a JSON data structure
 var cartopia = new google.maps.LatLng(45.512431,-122.65332);  /// 12th and se hawthorne ,"cartopia"
 var pod0 = new google.maps.LatLng(45.519934,-122.674499); // 3rd and sw washington
 var pod1 = new google.maps.LatLng(45.521143,-122.676215); // 5th and sw stark
@@ -7,7 +11,6 @@ var pod1 = new google.maps.LatLng(45.521143,-122.676215); // 5th and sw stark
 detectBrowser();
 
 var user_location = get_user_location();
-
 
 var map = new google.maps.Map(document.getElementById('map_canvas'), {
     zoom: 13,
@@ -25,16 +28,34 @@ new google.maps.Marker({
     map     : map
 });
 
+var closest_marker;
+var closest_marker_distance;
+var compute_distance = google.maps.geometry.spherical.computeDistanceBetween;
+
+
 $.getJSON("./data/carts.json",
     function(data) {
-        console.dir(data);
+        //console.dir(data);
+
+        // first guess for closest marker
+        // it is a hash, not an array
+        closest_marker          = data.markers[0];
+        closest_marker_distance = compute_distance(data.markers[0].getPosition(),user_location);
+
         $.each(data.markers, function(i,m){
+                if ( i > 0 ) { // don't check our first guess against itself
+                    var distance = google.maps.geometry.spherical.computeDistanceBetween(m.getPosition(),user_location);
+                    if ( distance < closest_marker_distance ) {
+                        closest_marker          = m;
+                        closest_marker_distance = distance;
+                    }
+
+                }
                 // just load 20 markers for testing
                 if ( i > 20 ) { return; }
 
                 console.log("adding marker " + m.lat + " " + m.lng + " i= " + i );
                 // TODO: lat and long are reversed in data file
-
 
                 var loc = new google.maps.LatLng(m.lng , m.lat );
                 var marker = new google.maps.Marker({
